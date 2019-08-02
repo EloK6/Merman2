a1bafcf00db3c35170c9063c9fbab6dbaa87b529
EloK6 <ecassignol@gmail.com>
2019-08-02 19:47:20 +0200

    background

---
+++ app.js
@@ -0,0 +1,46 @@
+// window.onload = function() {
+// 	document.getElementById("start-button").onclick = function() {
+// 		document.getElementById("title").style.display = "none";
+// 		document.getElementById("game-board").style.display = "block";
+// 		startGame();
+// 	};
+
+// };
+
+var img = new Image();
+img.src = "./images/bg_1200.svg";
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var backgroundImage = {
+	img: img,
+	x: -5,
+	speed: -1,
+
+	move: function() {
+		this.x += this.speed;
+		this.x %= canvas.width;
+	},
+
+	draw: function() {
+		ctx.drawImage(this.img, this.x, 0);
+		if (this.speed < 0) {
+			ctx.drawImage(this.img, this.x + canvas.width - 3, 0);
+		} else {
+			ctx.drawImage(this.img, this.x - this.img.width, 0);
+		}
+	}
+};
+
+function updateCanvas() {
+	backgroundImage.move();
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	backgroundImage.draw();
+
+	requestAnimationFrame(updateCanvas);
+}
+
+// start calling updateCanvas once the image is loaded
+img.onload = updateCanvas;
